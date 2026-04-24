15e4b9fa74b2cc42b01c0275110162a0eb58fe9a
Angie8669 <angelicaballin@gmail.com>
2026-04-23 19:05:11 -0700

    Added functions to create recipe and store into database. Added amounts and measurements to recipe form.

--- API_Routes.py
+++ API_Routes.py
@@ -1,3 +1,5 @@
+from asyncio.windows_events import NULL
+
 import bcrypt
 import credentials
 import pyodbc
@@ -7,7 +9,7 @@ from flask import request
 def initViews(app):
     @app.route("/api/getAllIngredients")
     def getAllIngredients():
-        query = "SELECT i.ingredientID, ingredientName, cost, STRING_AGG(CONVERT(NVARCHAR(max), measurement), ',') as possibleMeasurements FROM ingredients i LEFT JOIN ingredients_n_measurements inm ON i.ingredientID = inm.ingredientID group by i.ingredientID, ingredientName, cost"
+        query = "SELECT i.ingredientID, ingredientName, cost, STRING_AGG(CONVERT(NVARCHAR(max), measurement), ',') as possibleMeasurements FROM ingredients i LEFT JOIN ingredients_n_measurements inm ON i.ingredientID = inm.ingredientID group by i.ingredientID, ingredientName, cost ORDER BY i.ingredientName"
         response = queryDatabase(query)
         return response
 
@@ -50,6 +52,45 @@ def initViews(app):
         else:
             return "Incorrect Username or Password.", 400
 
+    @app.route("/api/createRecipe", methods=["POST"])
+    def createRecipe():
+        userID = request.json["userID"]
+        recipeName = request.json["recipeName"]
+        imageURL = request.json["imageURL"]
+        instructions = request.json["instructions"]
+        ingredients = request.json["ingredients"]
+        equipment = request.json["equipment"]
+
+        query = "SELECT * FROM users WHERE userID = ?"
+        data = (userID)
+        response = queryDatabase(query, data)
+        if len(response) == 0:
+            return "User does not exist.", 400
+
+        if instructions == "" or instructions == None:
+            return "Invalid Instructions.", 400
+
+        if len(ingredients) == 0 or ingredients == None:
+            return "Invalid Ingredients.", 400
+
+        query = "INSERT INTO recipes (recipeName, instructions, img, userID) VALUES (?, ?, ?, ?)"
+        data = (recipeName, instructions, imageURL, userID)
+        recipeID = queryDatabaseInsert(query, data)
+
+        if not recipeID:
+            return "Unable to insert recipe.", 400
+
+        for ingredient in ingredients:
+            query = "INSERT INTO recipes_n_ingredients (recipeID, ingredientID, measurement, amount) VALUES (?, ?, ?, ?)"
+            data = (recipeID, ingredient["ingredientID"], ingredient["measurement"], ingredient["amount"])
+            queryDatabaseInsert(query, data)
+
+        for equipmentVal in equipment:
+            query = "INSERT INTO recipes_n_equipment (recipeID, equipment) VALUES (?, ?)"
+            data = (recipeID, equipmentVal)
+            queryDatabaseInsert(query, data)
+
+        return "{\"response\":\"success\"}"
 
 
 password = credentials.password
@@ -99,9 +140,10 @@ def queryDatabaseInsert(query, data):
         )
         cur = conn.cursor()
         cur.execute(query, data)
+        insertID = cur.execute("SELECT @@IDENTITY AS id;").fetchone()[0]
         conn.commit()
         cur.close()
-        return True
+        return insertID
     except Exception as e:
         print(f"Database error: {e}")
         raise
--- static/scripts/recipeForm.js
+++ static/scripts/recipeForm.js
@@ -9,13 +9,37 @@ $(document).ready(function() {
 
     function addIngredientDropdown() {
         numIngredients++;
-        let dropdown = "<select id='ingredientDropDown"+numIngredients+"' class='selectpicker' data-live-search='true'>";
-        dropdown += "<option value=''></option>";
+
+        let ingredientHTML = "<input type='number' step='0.0625' id='amount"+numIngredients+"'/>"
+        ingredientHTML += "<select id='measurementDropDown"+numIngredients+"'>";
+        ingredientHTML += "<option value=''></option>";
+
+        ingredientHTML += "</select>";
+        ingredientHTML += "<select id='ingredientDropDown"+numIngredients+"'>";
+        ingredientHTML += "<option value=''></option>";
         for(const ingredient of ingredients) {
-            dropdown += "<option value='"+ingredient.ingredientID+"'>"+ingredient.ingredientName+"</option>";
+            ingredientHTML += "<option value='"+ingredient.ingredientID+"'>"+ingredient.ingredientName+"</option>";
         }
-        dropdown += "</select>";
-        $("#ingredientList").append(dropdown);
+        ingredientHTML += "</select>";
+        $("#ingredientList").append(ingredientHTML);
+
+        $("#ingredientDropDown"+numIngredients).on("change", function(event){
+
+            let i = event.target.id.replace("ingredientDropDown", "");
+            console.log(i)
+            let curVal = $("#ingredientDropDown"+i).val();
+            for(const ingredient of ingredients) {
+                if(ingredient.ingredientID == curVal) {
+                    const measurements = ingredient.possibleMeasurements.split(",");
+                    console.log(measurements);
+                    $("#measurementDropDown"+i).empty();
+                    for(const measurement of measurements) {
+                        $("#measurementDropDown"+i).append($("<option></option>").attr("value", measurement).text(measurement));
+                        console.log(measurement);
+                    }
+                }
+            }
+        });
     }
 
     $.ajax({
@@ -74,16 +98,47 @@ $(document).ready(function() {
     });
 
     $("#submit").on("click", function(){
-       var imageURL = $("#ImageURL").val();
+       var recipeName = $("#recipeName").val();
+       var imageURL = $("#imageURL").val();
        var instructions = $("#instructions").val();
        var ingredientVals = [];
        var equipmentVals = [];
+
        for(var i = 1; i <= numIngredients; i++) {
-            ingredientVals.push($("#ingredientDropDown"+i).val());
+            let ingredient = {};
+            ingredient["ingredientID"] = $("#ingredientDropDown"+i).val();
+            ingredient["amount"] = $("#amount"+i).val();
+            ingredient["measurement"] = $("#measurementDropDown"+i).val();
+            ingredientVals.push(ingredient);
        }
        for(var i = 1; i <= numEquipment; i++) {
             equipmentVals.push($("#equipmentDropDown"+i).val());
        }
+
+       let recipe = {};
+       recipe["userID"] = userID;
+       recipe["recipeName"] = recipeName;
+       recipe["imageURL"] = imageURL;
+       recipe["instructions"] = instructions;
+       recipe["ingredients"] = ingredientVals;
+       recipe["equipment"] = equipmentVals;
+       console.log(recipe);
+
+       $.ajax({
+            url: "/api/createRecipe",
+            method: "POST",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify(recipe),
+            success: function (data) {
+                console.log(data);
+            },
+            error: function (xhr, status, err) {
+                console.error("Error:", status, err);
+                $("#errorMessage").text(xhr.responseText);
+                return false;
+            }
+        });
     });
 
 });
